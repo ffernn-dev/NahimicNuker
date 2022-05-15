0527fada43afd1eba354fbc98c8aa264374b8e6c
ffernn <hihi.fernguy@gmail.com>
2022-05-16 07:28:45 +1000

    Starting to check what needs to be deleted

--- NahimicNuker.py
+++ NahimicNuker.py
@@ -1,8 +1,30 @@
 import sys
 import psutil
 import ctypes
+import os
 import logging
+import win32api
+import win32serviceutil
 from ui_main import *
+
+def nuke():
+        isrunning = "NahimicService.exe" in (p.name() for p in psutil.process_iter())
+        if isrunning:
+            logging.info("NahimicService is running")
+        sys32exeinstalled = os.path.exists("C:\Windows\System32\\NahimicService.exe")
+        if sys32exeinstalled:
+            logging.info("Found NahimicService.exe in System32")
+        
+        try:
+            serviceisrunning = True if win32serviceutil.QueryServiceStatus("NahimicService")[1] == 4 else False
+            print(serviceisrunning)
+        except Exception as ex:
+            logging.debug(ex)
+            servicefound = False
+        else:
+            logging.info("Found the NahimicService Service")
+            servicefound = True
+
 
 class Application(Ui_MainWindow):
     def __init__(self,window):
@@ -13,6 +35,7 @@ class Application(Ui_MainWindow):
     def GoClicked(self):
         print(self.MoboManufacturercomboBox.currentText())
         print(self.BlacklistIDsCheckbox.isChecked())
+        nuke()
     
     def onMoboBoxChanged(self):
         self.GoButton.setEnabled(True)
@@ -23,6 +46,8 @@ if __name__ == "__main__":
         ctypes.windll.user32.MessageBoxW(0, "This software needs to be run as administrator", "Oh no", 0)
         sys.exit()
     
+    logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(message)s')
+
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
 
@@ -32,6 +57,4 @@ if __name__ == "__main__":
 
     sys.exit(app.exec())
 
-def nuke():
-    isrunning = "NahimicService.exe" in (p.name() for p in psutil.process_iter())
-    
+
